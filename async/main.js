378f0b71e42de7fa30c163ab7009427fc137ffbb
AlenaNik <alena.nik.digital@gmail.com>
2019-05-29 14:10:23 +0200

    first one

--- async/main.js
+++ async/main.js
@@ -4,8 +4,29 @@
 // Don't repeat yourself
 
 
-// Version promesas (usa then)
+    fetch('./config.json')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(myJson) {
+            console.log(myJson);
+        }).then(function(dataFilePath) {
+        console.log(JSON.stringify(dataFilePath));
+        }).catch(function(error) {
+        console.log('Hubo un problema con la petición Fetch:' + error.message);
+        });
 
 
 
 // Version async/await
+
+solve()
+async function solve() {
+    let res = await fetch('config.json')
+    let data = await res.json()
+    let res2 = await fetch(data.data.dataFilePath);
+    let data2 = await res2.json();
+    console.log(data2);
+}
+
+
